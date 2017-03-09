c95f54d3e401ad476b112cdfcde8736f12460e92
Kunyuan Hsieh <kunyuanmac@yahoo.com.tw>
2017-03-09 17:24:16 +0800

    Update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-  channelId:1504869891,
-  channelSecret:14baadc381cd685094fdb7bcb8567c4f,
-  channelAccessToken:ROlz+hP10LcWe+Ptwk6+VcCi+CxwntCG6IFcV0HcMTiDHN5jp0+ECOZVfCXec6o1vUM/1OdrDSWn5mSQ9ZKt0vYaotVT7kgyjSua81RAvGogj8yjztIyEL5GNsmo8jHstpT1RKcuObiUiN6ORNo6gQdB04t89/1O/w1cDnyilFU=
+  channelId: 1504869891,
+  channelSecret: 14baadc381cd685094fdb7bcb8567c4f,
+  channelAccessToken: ROlz+hP10LcWe+Ptwk6+VcCi+CxwntCG6IFcV0HcMTiDHN5jp0+ECOZVfCXec6o1vUM/1OdrDSWn5mSQ9ZKt0vYaotVT7kgyjSua81RAvGogj8yjztIyEL5GNsmo8jHstpT1RKcuObiUiN6ORNo6gQdB04t89/1O/w1cDnyilFU=
 });
 
 bot.on('message', function(event) {
