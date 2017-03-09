fc698e8927ac0b645f9cb1c164c31776ba6777cb
Kunyuan Hsieh <kunyuanmac@yahoo.com.tw>
2017-03-09 17:08:35 +0800

    no message

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+var linebot = require('linebot');
 var express = require('express');
-var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+var bot = linebot({
+  channelId:1504869891,
+  channelSecret:14baadc381cd685094fdb7bcb8567c4f,
+  channelAccessToken:ROlz+hP10LcWe+Ptwk6+VcCi+CxwntCG6IFcV0HcMTiDHN5jp0+ECOZVfCXec6o1vUM/1OdrDSWn5mSQ9ZKt0vYaotVT7kgyjSua81RAvGogj8yjztIyEL5GNsmo8jHstpT1RKcuObiUiN6ORNo6gQdB04t89/1O/w1cDnyilFU=
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
 });
 
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
 
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
